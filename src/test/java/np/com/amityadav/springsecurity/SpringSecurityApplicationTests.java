bdef031de2fa034b1909ca9c71b48d9f82125e2d
amityadav137 <amit.yadav137@outlook.com>
2023-09-13 16:19:38 -0500

    Project Initial Setup

---
+++ src/test/java/np/com/amityadav/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.amityadav.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
