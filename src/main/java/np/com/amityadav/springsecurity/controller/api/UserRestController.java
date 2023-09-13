63ef043555ce4ed3bc73574c85a43bf50faa51ef
frensdude <frensdude4all@gmail.com>
2023-09-13 18:41:32 -0500

    Change to UserRestController

--- src/main/java/np/com/amityadav/springsecurity/controller/api/UserRestController.java
+++ src/main/java/np/com/amityadav/springsecurity/controller/api/UserRestController.java
@@ -1,4 +1,19 @@
 package np.com.amityadav.springsecurity.controller.api;
 
+import np.com.amityadav.springsecurity.dto.UserDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/users")
 public class UserRestController {
+
+    @PostMapping
+    public String createUser(@RequestBody UserDto userDto){
+        return "User Created";
+    }
+
+    @GetMapping
+    public String getUsers(){
+        return "All Users";
+    }
 }
